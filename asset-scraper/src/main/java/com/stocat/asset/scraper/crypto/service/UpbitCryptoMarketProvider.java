ba186ce190de20ce5b5f690d24223fcd70f4cae8
xEzIxX <by1094@gachon.ac.kr>
2026-03-09 19:45:12 +0900

    refactor: 더 이상 사용되지 않는 @Deprecated 된 메서드 삭제

--- asset-scraper/src/main/java/com/stocat/asset/scraper/crypto/service/UpbitCryptoMarketProvider.java
+++ asset-scraper/src/main/java/com/stocat/asset/scraper/crypto/service/UpbitCryptoMarketProvider.java
@@ -2,10 +2,6 @@ package com.stocat.asset.scraper.crypto.service;
 
 import com.stocat.asset.scraper.crypto.dto.response.MarketEventDetailResponse;
 import com.stocat.asset.scraper.dto.MarketInfo;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -51,90 +47,4 @@ public class UpbitCryptoMarketProvider {
                         detail.marketEvent().caution().get(TRADING_VOLUME_SOARING)
                 );
     }
-
-    /**
-     * SOARING 이벤트 종목 중 랜덤 샘플 n 개를 반환합니다. 만약 SOARING 종목이 n개 미만이라면, 전체 코드에서 채워 넣습니다. *** 경고 기준으로는 거래량이 없는 코인이 많아서 사용하지
-     * 않습니다. ***
-     */
-    @Deprecated
-    public Set<MarketInfo> getDailyCrypto(int n) {
-        return upbitWebClient.get()
-                .uri(uri -> uri.path("/v1/market/all")
-                        .queryParam("isDetails", "true")
-                        .build())
-                .header(HttpHeaders.ACCEPT, "application/json")
-                .retrieve()
-                .bodyToFlux(MarketEventDetailResponse.class)
-                .collectList()
-                .map(list -> pickOrFill(list, n))
-                .map(this::toMarketInfo)
-                .block();
-    }
-
-    @Deprecated
-    private Set<MarketInfo> toMarketInfo(List<MarketEventDetailResponse> response) {
-        return response.stream()
-                .map(detail -> MarketInfo.createCrypto(detail.market(), detail.koreanName(), detail.englishName()))
-                .collect(Collectors.toSet());
-    }
-
-    /**
-     * 1) allDetails 에서 marketEvent.caution 에 "SOARING" 포함된 market 코드만 뽑아서 셔플 2) allDetails 에서 모든 market 코드만 뽑아서 셔플 3) 두
-     * 스트림을 합친 뒤 distinct() -> limit 개수만큼 반환
-     */
-    @Deprecated
-    private List<MarketEventDetailResponse> pickOrFill(Collection<MarketEventDetailResponse> allDetails, int limit) {
-        List<MarketEventDetailResponse> marketList = new ArrayList<>(getVolumeSoaring(allDetails));
-        Collections.shuffle(marketList);
-
-        if (marketList.size() >= limit) {
-            return marketList.stream()
-                    .limit(limit)
-                    .collect(Collectors.toList());
-        }
-
-        int remaining = limit - marketList.size();
-        List<MarketEventDetailResponse> otherCaution = getSoaringWithoutVolume(allDetails, marketList);
-        Collections.shuffle(otherCaution);
-
-        otherCaution = otherCaution.stream().limit(remaining).toList();
-        marketList.addAll(otherCaution);
-
-        if (marketList.size() >= limit) {
-            return marketList.stream()
-                    .limit(limit)
-                    .collect(Collectors.toList());
-        }
-
-        remaining = limit - marketList.size();
-        List<MarketEventDetailResponse> reamainList = allDetails.stream()
-                .filter(list -> !marketList.contains(list))
-                .limit(remaining)
-                .toList();
-
-        marketList.addAll(reamainList);
-        return marketList;
-    }
-
-    @Deprecated
-    private List<MarketEventDetailResponse> getSoaringWithoutVolume(Collection<MarketEventDetailResponse> allDetails,
-                                                                    List<MarketEventDetailResponse> volSoaring) {
-        return allDetails.stream()
-                .filter(md -> md.marketEvent() != null && md.marketEvent().caution() != null)
-                .filter(md -> md.marketEvent().caution().entrySet().stream()
-                        .anyMatch(e -> Boolean.TRUE.equals(e.getValue())))
-                .filter(md -> !volSoaring.contains(md))
-                .distinct()
-                .toList();
-    }
-
-    @Deprecated
-    private List<MarketEventDetailResponse> getVolumeSoaring(Collection<MarketEventDetailResponse> allDetails) {
-        return allDetails.stream()
-                .filter(md -> md.marketEvent() != null && md.marketEvent().caution() != null)
-                .filter(md -> Boolean.TRUE.equals(md.marketEvent().caution().get(TRADING_VOLUME_SOARING)))
-                .distinct()
-                .toList();
-    }
-
 }
